37cf0e714d07633c531d216585a7b23aa4cb600f
toandd-it <toandd.it@gmail.com>
2022-04-19 03:52:30 +0700

    update

--- service_create.sh
+++ service_create.sh
@@ -1,10 +1,12 @@
+sudo firewall-cmd --zone=public --add-port=8090/tcp --permanent
+sudo firewall-cmd --reload
 sudo touch /usr/lib/systemd/system/phpWs.service
 echo '[Unit]' >> /usr/lib/systemd/system/phpWs.service
 echo 'Description=PHP Websocket service' >> /usr/lib/systemd/system/phpWs.service
 echo 'After=network.target' >> /usr/lib/systemd/system/phpWs.service
 echo '' >> /usr/lib/systemd/system/phpWs.service
 echo '[Service]' >> /usr/lib/systemd/system/phpWs.service
-echo 'ExecStart=/usr/local/lsws/lsphp73/bin/php /your-dir/wss.php' >> /usr/lib/systemd/system/phpWs.service
+echo 'ExecStart=/usr/local/lsws/lsphp73/bin/php /_your_dir/wss.php' >> /usr/lib/systemd/system/phpWs.service
 echo 'Restart=always' >> /usr/lib/systemd/system/phpWs.service
 echo 'User=nobody' >> /usr/lib/systemd/system/phpWs.service
 echo '' >> /usr/lib/systemd/system/phpWs.service
@@ -14,9 +16,6 @@ sudo chmod 644 /usr/lib/systemd/system/phpWs.service
 
 sudo ln -s /usr/lib/systemd/system/phpWs.service /etc/systemd/system/
 sudo ls -l /usr/lib/systemd/system/phpWs.service
-
-sudo firewall-cmd --zone=public --add-port=8090/tcp --permanent
-sudo firewall-cmd --reload
 
 sudo systemd-analyze verify phpWs.service 
 sudo systemctl daemon-reload
